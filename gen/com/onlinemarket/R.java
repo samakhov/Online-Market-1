450bd3e5ad493f768a09898a048bbecb9e5cc0b0
alireza.elamifar <a.eslamifar@hotmail.com>
2014-05-21 02:15:35 +0430

    Splash.xml

--- gen/com/onlinemarket/R.java
+++ gen/com/onlinemarket/R.java
@@ -27,7 +27,7 @@ public final class R {
         public static final int loader_3=0x7f020003;
         public static final int loader_4=0x7f020004;
         public static final int loader_5=0x7f020005;
-        public static final int logo=0x7f020006;
+        public static final int logo1=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
